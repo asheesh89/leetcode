fdb71d437e00b501d2455b1bd2b8d54c769dd9a6
Asheesh Kumar <kumar.asheesh89@gmail.com>
2024-07-17 16:10:35 -0500

    while loop removed

--- src/com/aku/test/TestMain.java
+++ src/com/aku/test/TestMain.java
@@ -3,10 +3,8 @@ package com.aku.test;
 public class TestMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		while(true) {
+		// TODO Auto-generated method stub\
 			System.out.println("Hello Asheesh");
-		}
 
 	}
 
